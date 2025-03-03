9e5658024070b091433d6ae1d1005920f72d45d0
Casper <casper141@gmail.com>
2025-03-03 11:14:55 +0200

    part 1 iteration 3-4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // pages & components
+import Navbar from "./components/Navbar";
 import Home from "./pages/HomePage";
 import AddJobPage from "./pages/AddJobPage";
-import Navbar from "./components/Navbar";
-import NotFoundPage from "./pages/NotFoundPage"
+import JobPage from "./pages/JobPage";
+import EditJobPage from "./pages/EditJobPage";
+import NotFoundPage from "./pages/NotFoundPage";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
@@ -20,6 +22,8 @@ const App = () => {
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
               <Route path="/add-job" element={<AddJobPage />} />
+              <Route path="/jobs/:id" element={<JobPage />} />
+              <Route path="/edit-job/:id" element={<EditJobPage />} />
               <Route path='*' element={<NotFoundPage />} />
             </Routes>
           </div>
@@ -27,5 +31,5 @@ const App = () => {
       </div>
     );
   }
-  
+
   export default App;
